fd316e0d76dde04375a44be844f3edf7d3f277f9
M.A.Omar Faruq <omarfaruq.reza02@gmail.com>
2025-07-28 13:10:41 +0600

    cleaned

--- selenium with oop/Run_tests.py
+++ selenium with oop/Run_tests.py
@@ -3,11 +3,15 @@ import time
 from test_login import valid_login
 from search_page import Search
 
-
+#Navigate & login
 driver = valid_login()
-
 time.sleep(3)
+
+#search
 Search(driver).search("Awesome")
 time.sleep(3)
+
+
+
 
 driver.quit()
